efdd3ff6ad364859326b060727d3bea8e29d3289
AndiFTIUKDW <andi.putrayana@ti.ukdw.ac.id>
2022-12-02 09:48:12 +0700

    membenarkan nomor 1

--- 1_A_71220822.py
+++ 1_A_71220822.py
@@ -1,22 +1,22 @@
-def penghitung_kata(kalimat,kata):
+def hitungKata(kalimat,kata):
     x = kalimat.count(kata)
     return x
-def pengganti_kata(kalimat,terubah,perubah):
+def ubahKata(kalimat,terubah,perubah):
     x = kalimat.replace(terubah,perubah)
     return x
 print("Pilihan Menu :")
 print("1. Hitung kata")
 print("2. Ubah Kata")
-pilihan = int(input("Pilihan anda :"))
-kalimat = str(input("Masukan sebuah kalimat/kata :"))
+pilihan = int(input("Pilihan anda : "))
+kalimat = str(input("Masukan sebuah kalimat/kata : "))
 if pilihan == 1:
-    kata = str(input("Masukan kata yang ingin dihitung :"))
-    h = penghitung_kata(kalimat,kata)
+    kata = str(input("Masukan kata yang ingin dihitung : "))
+    h = hitungKata(kalimat,kata)
     print("terdapat sebanyak",h,"kata",kata,"di dalam kalimat")
 elif pilihan == 2:
     ubah = str(input("Masukan kata yang ingin di ubah : "))
     kataperubah = str(input("Masukan kata pengganti : "))
-    h = pengganti_kata(kalimat,ubah,kataperubah)
+    h = ubahKata(kalimat,ubah,kataperubah)
     print("String berhasil dirubah menjadi :",h)
 else:
     print("Pilihan yang anda input tidak terdaftar di daftar pilihan")
